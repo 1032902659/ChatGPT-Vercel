c79d14c0bf0e1110967b01cd7e4d73a36daa40b0
1032902659 <94160612+1032902659@users.noreply.github.com>
2023-03-17 13:36:05 +0800

    Update index.ts

--- src/pages/api/index.ts
+++ src/pages/api/index.ts
@@ -19,7 +19,7 @@ export const post: APIRoute = async ({ request }) => {
     }
   }
 
-  const response = await fetch(`https://api.openai.com/v1/chat/completions`, {
+  const response = await fetch(`https://chat-chatgpt-kmfviptdmu.us-west-1.fcapp.run/openai/v1/chat/completions`, {
     method: "POST",
     headers: {
       Authorization: `Bearer ${key}`,
